4eaeabf12d31dd578cf89bf8e7851d9f7e83eab5
Dave Cross <dave@davecross.co.uk>
2025-02-19 17:35:04 +0000

    Add search_live and find_live methods to role class
    Fixes #6
    
    Add `search_live` and `find_live` methods to `LiveFlag` role
    
    * Add `search_live` method that overrides `is_live` key in `$search` to true and passes the call to the original `search` method.
    * Add `find_live` method that overrides `is_live` key in `$search` to true and passes the call to the original `find` method.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/davorg/adserver/issues/6?shareId=XXXX-XXXX-XXXX-XXXX).

--- AdServer/lib/AdServer/Role/LiveFlag.pm
+++ AdServer/lib/AdServer/Role/LiveFlag.pm
@@ -2,4 +2,18 @@ package AdServer::Role::LiveFlag;
 
 use Moose::Role;
 
+sub search_live {
+    my ($self, $search, $attrs) = @_;
+    $search //= {};
+    $search->{is_live} = 1;
+    return $self->search($search, $attrs);
+}
+
+sub find_live {
+    my ($self, $search, $attrs) = @_;
+    $search //= {};
+    $search->{is_live} = 1;
+    return $self->find($search, $attrs);
+}
+
 1;
